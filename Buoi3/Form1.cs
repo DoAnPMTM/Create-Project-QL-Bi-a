c27d697d1f67330cff8164731da216b5ba095cbe
h-ieu1 <minkhieu1811@gmail.com>
2024-07-16 10:41:16 +0700

    create project

---
+++ Buoi3/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ThuVien;
+
+namespace Buoi3
+{
+    public partial class Form1 : Form
+    {
+        SQL tv = new SQL("Data Source=A109PC32;Initial Catalog=B3;Integrated Security=True");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            testConnect();
+            string Name = textBox1.Text;
+            string Pass = textBox2.Text;
+
+            string sql = "select * from Account where Name = '" + Name + "' and Pass = '" + Pass + "'";
+
+            DataTable dtTbl = tv.ExcuteQuery(sql);
+            if (dtTbl.Rows.Count > 0)
+            {
+                MessageBox.Show("Đăng Nhập Thành Công!!");
+            }
+        }
+
+        private void testConnect()
+        {
+            if (tv.TestConnect())
+            {
+                MessageBox.Show("Kết nối thành công");
+            }
+            else
+            {
+                MessageBox.Show("Kết nối thất bại");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string Name = textBox1.Text;
+            string Pass = textBox2.Text;
+            string query = "insert into Account values ('"+Name+"', '"+Pass+"')";
+            if (tv.Insert(query))
+            {
+                MessageBox.Show("Thêm Thành Công");
+            }
+            else
+            {
+                MessageBox.Show("Thêm Thất Bại");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ThuVien/SQL.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ThuVien
+{
+    public class SQL
+    {
+        public SqlConnection con;
+
+        public SQL(string ConStr)
+        {
+            con = new SqlConnection(ConStr);
+        }
+        public void CreateConnection(string pConnectionString)
+        {
+            con = new SqlConnection(pConnectionString);            
+        }
+
+        public bool TestConnect()
+        {
+            try
+            {
+                con.Open();
+                con.Close();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public DataTable ExcuteQuery(String pQuery)
+        {
+            DataTable DT = new DataTable();
+            con.Open();
+            using (SqlCommand cmd = new SqlCommand(pQuery, con))
+            {
+                using (SqlDataAdapter adp = new SqlDataAdapter(cmd))
+                {
+                    adp.Fill(DT);
+                }
+            }
+            con.Close();
+            return DT;
+        }
+
+        public bool Insert(String pQuery)
+        {
+            try
+            {
+                con.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter();
+                adapter.InsertCommand = new SqlCommand(pQuery, con);
+                adapter.InsertCommand.ExecuteNonQuery();
+                con.Close();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool Update(String pQuery)
+        {
+            try
+            {
+                con.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter();
+                adapter.UpdateCommand = new SqlCommand(pQuery, con);
+                adapter.UpdateCommand.ExecuteNonQuery();
+                con.Close();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+    }
+}
